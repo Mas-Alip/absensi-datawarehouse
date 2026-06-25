b052d2c52a6309f4b7ea13fd7115a19a0d531b1b
Alif Adji <alifadji135@gmail.com>
2026-06-25 11:32:24 +0700

    Make office IP ranges configuration dynamic

--- config/office.php
+++ config/office.php
@@ -1,11 +1,26 @@
 <?php
 
 return [
+    /*
+    |--------------------------------------------------------------------------
+    | Status Pembatasan Jaringan Kantor
+    |--------------------------------------------------------------------------
+    |
+    | Jika diatur ke true, presensi dengan status HADIR hanya bisa dilakukan 
+    | di dalam jaringan IP kantor yang terdaftar di bawah ini.
+    |
+    */
     'enabled' => env('OFFICE_NETWORK_RESTRICTION', true),
 
-    'office_ip_ranges' => [
-        '192.168.0.0/24',
-        '192.168.1.0/24',
-        '10.0.0.0/24',
-    ],
+    /*
+    |--------------------------------------------------------------------------
+    | Daftar IP / Subnet Kantor yang Diizinkan
+    |--------------------------------------------------------------------------
+    |
+    | Nilai ini akan membaca variabel 'OFFICE_IP_RANGES' dari Railway.
+    | Kamu bisa memasukkan beberapa IP sekaligus dengan memisahkannya menggunakan koma (,).
+    | Jika variabel di Railway kosong, sistem otomatis menggunakan default IP lokal.
+    |
+    */
+    'office_ip_ranges' => explode(',', env('OFFICE_IP_RANGES', '192.168.0.0/24,192.168.1.0/24,10.0.0.0/24,127.0.0.1')),
 ];
